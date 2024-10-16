1ebb5a0f0ea2efc1af924f546df3d0c2123f6460
Terrana Willma <terrana.w@gmail.com>
2024-10-16 17:56:27 +0700

    Added JWT token for user authentication

--- controllers/Auth-controller.js
+++ controllers/Auth-controller.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require("../models/User");
 
@@ -26,13 +28,18 @@ module.exports = {
         const user = await User.findOne({ username });
         if (!user) return res.status(404).json({ message: "User not found" });
 
-
         //cek pw lalu compare dengan pw hashing
         const checkPassword = await bcrypt.compareSync(password, user.password);
         if (!checkPassword) return res.status(401).json({ message: "Incorrect username or password" });
 
+        const token = jwt.sign(
+            { username: user.username, email: user.email }, //payload
+            process.env.JWT_KEY //secretKey(simpan di env)
+        );
+
         res.json({
-            message: "successfully logged in"
+            message: "successfully logged in",
+            token,
         });
 
     },
